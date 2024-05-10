0fa6e1ff8b712670263ac104d782b154d8f76474
ArdorHoon <didgnsah7320@gmail.com>
2024-05-10 11:17:27 +0900

    #1 - QueryDsl 디펜던시 및 설정  gitignore 추가

--- src/main/java/com/fastcampus/projectboard/repository/ArticleRepository.java
+++ src/main/java/com/fastcampus/projectboard/repository/ArticleRepository.java
@@ -1,9 +1,35 @@
 package com.fastcampus.projectboard.repository;
 
 import com.fastcampus.projectboard.domain.Article;
+import com.fastcampus.projectboard.domain.QArticle;
+import com.querydsl.core.types.dsl.DateTimeExpression;
+import com.querydsl.core.types.dsl.StringExpression;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
-public interface ArticleRepository extends JpaRepository<Article, Long> {
+public interface ArticleRepository
+        extends JpaRepository<Article, Long>,
+        //Article 안에 있는 모든 필드에 대한 기본 검색 기능을 제공해준다.
+        QuerydslPredicateExecutor<Article>,
+        //우리가 입맞에 맞는 검색 기능을 추가하기 위해 사용
+        QuerydslBinderCustomizer<QArticle>
+{
+
+    //애를 구현해주어야 함
+    @Override
+    default void customize(QuerydslBindings bindings, QArticle root){
+        bindings.excludeUnlistedProperties(true);
+        bindings.including(root.title, root.content,root.hashtag, root.createdAt, root.createdBy);
+        //bindings.bind(root.title).first(StringExpression::likeIgnoreCase); // like '${value}'
+        bindings.bind(root.title).first(StringExpression::containsIgnoreCase); // like '%%${value}%'
+        bindings.bind(root.hashtag).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.content).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.createdAt).first(DateTimeExpression::eq);
+        bindings.bind(root.createdBy).first(StringExpression::containsIgnoreCase);
+
+    }
 }
